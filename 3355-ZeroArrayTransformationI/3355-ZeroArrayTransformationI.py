7e7db78aa240ed804cbb62131700b5c190e4ba19
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2025-05-20 13:30:08 +0530

    File updated at 5/20/2025, 1:30:07 PM

--- 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.py
+++ 3355-ZeroArrayTransformationI/3355-ZeroArrayTransformationI.py
@@ -1,17 +1,34 @@
-# Last updated: 5/20/2025, 1:27:39 PM
+# Last updated: 5/20/2025, 1:30:07 PM
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
-        n=len(nums)
-        freq=[0]*(n+1)
-        for l,r in queries:
-            freq[l]+=1
-            if r+1 < n:
-                freq[r+1] -=1
-        
-        for i in range(1,n):
-            freq[i]+=freq[i-1]
-        for i in range(n):
-            if nums[i]>freq[i]:
-                return False
+        # Given an array we need to validate if after all the queries all entries become zero.
+        # For every query l,r choose a subset and deccrease all values by 1
+        #   since its a subset we can choose all the indexes for which values are non-zero
+        # 
+        # preprocess and put all the indexes with values 0 in a set. 
+        # process the range l,r
+        # if value becomes 0 add index to set. 
+        # if after all query len of set is same as n True. 
+        '''
+        arr = deepcopy(nums)
+        _sum = sum(nums)
+        for l,r in queries :
+            for i in range(l,r+1) :
+                if arr[i] == 0 : continue
+                arr[i] -= 1
+                _sum -= 1
+        return True if _sum == 0 else False
+        '''
+        arr = [0]*(len(nums)+1) 
 
+        for l,r in queries :
+            arr[l] += 1
+            arr[r+1] -= 1
+        rsum = 0
+        for i in range(len(nums)) :
+            rsum += arr[i]
+            if nums[i] > rsum :
+                return False
         return True
+
+import atexit; atexit.register(lambda: open("display_runtime.txt", "w").write("0"))

b69dc972cb246f389696a2be4befbc0645244044
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2025-05-23 08:24:47 +0530

    Create file

---
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -0,0 +1,21 @@
+# Last updated: 5/23/2025, 8:24:46 AM
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last = m+n-1
+        while m>0 and n>0:
+            if nums1[m-1]>nums2[n-1]:
+                nums1[last]=nums1[m-1]
+                m-=1
+            else:
+                nums1[last]=nums2[n-1]
+                n-=1
+            last-=1
+        
+        #fill num1 with leftover b=num2 elements
+        while n>0:
+            nums1[last]=nums2[n-1]
+            last-=1
+            n-=1

5fee40e3b94354e18977002ffd7a09d2db18ed21
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2025-05-23 13:26:11 +0530

    Create file

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,12 @@
+# Last updated: 5/23/2025, 1:26:10 PM
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]

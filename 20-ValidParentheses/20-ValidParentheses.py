1ae4112f95c7d019b234fb6016cedd52f3949610
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2025-05-24 09:50:56 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,9 @@
+# Last updated: 5/24/2025, 9:50:55 AM
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while len(s)>0:
+            l=len(s)
+            s=s.replace('()','').replace('[]','').replace('{}','')
+            if l==len(s): 
+                return False
+        return True

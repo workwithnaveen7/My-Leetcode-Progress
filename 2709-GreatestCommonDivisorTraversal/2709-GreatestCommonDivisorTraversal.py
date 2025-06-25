3bed51f1b5d8dff2b0e7df8283c1bff8e782670d
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2025-06-25 22:37:13 +0530

    Create 2709-GreatestCommonDivisorTraversal

---
+++ 2709-GreatestCommonDivisorTraversal/2709-GreatestCommonDivisorTraversal.py
@@ -0,0 +1,18 @@
+# Last updated: 6/25/2025, 10:37:14 PM
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        if len(nums)==1:
+            return True 
+        if 1 in nums: 
+            return False
+
+        nums=sorted(set(nums),reverse=True)
+        if (n:=len(nums))==1: 
+            return True
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if gcd(nums[i],nums[j])-1:
+                    nums[j]*=nums[i]
+                    break
+            else: return False
+        return True
